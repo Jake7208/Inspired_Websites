a33ca06461d39e5e59dd1c0491d54d22945ce32d
Jake7208 <jake7208@gmail.com>
2023-09-17 01:46:33 -0600

    updated stuff.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,104 @@
-const lenis = new Lenis()
+const lenis = new Lenis();
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-
-requestAnimationFrame(raf)
-
-const tl = gsap.timeline({ paused: true, ease: "power1.inOut" });
-
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('.nav-menu');
-const closeMenu = document.querySelector('.close-menu');
-
-tl.to(navMenu, {
-  clipPath: 'polygon(100% 0, 0 0, 0 100%, 100% 100%)',
-  duration: 0.5, // Adjust the duration as needed
+lenis.on("scroll", (e) => {
+  console.log(e);
 });
 
-hamburger.addEventListener('click', () => {
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+gsap.registerPlugin(ScrollTrigger);
+const mask = document.querySelectorAll(".mask p");
+const expertise = document.querySelector("#expertise");
+const scrollMask = document.querySelector(".mask h1");
+const title = document.querySelector("#title h1");
+const nav = document.querySelector("nav");
+
+const loadIn = gsap.timeline({
+  defaults: { duration: 1, ease: Power2.ease },
+});
+
+loadIn.from(nav, 0.8, {
+  opacity: 0,
+  y: "-20%",
+});
+loadIn.from(title, 0.5, {
+  opacity: 0,
+  x: "-10%",
+});
+
+loadIn.from(
+  mask,
+  1,
+  {
+    y: "-100%",
+  },
+  "< 0.5"
+);
+
+const tl = gsap.timeline({ paused: true });
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const closeMenu = document.querySelector(".close-menu");
+
+tl.to(navMenu, {
+  clipPath: "polygon(100% 0, 0 0, 0 100%, 100% 100%)",
+  duration: 0.2,
+  ease: Power2.easeInOut,
+});
+
+hamburger.addEventListener("click", () => {
   tl.play();
 });
 
-closeMenu.addEventListener('click', () => {
+closeMenu.addEventListener("click", () => {
   tl.reverse();
+});
+
+const tl2 = gsap.timeline();
+
+tl2.fromTo(
+  nav,
+  {
+    background: "var(--nav-color)",
+    transition: "background 0.5s ease",
+  },
+  {
+    background: "var(--background-trans-color)",
+  }
+),
+  ">";
+
+tl2.fromTo(
+  expertise,
+  {
+    background: "var(--background-color)",
+    transition: "background 0.5s ease",
+  },
+  {
+    background: "var(--background-trans-color)",
+  }
+);
+
+tl2.fromTo(
+  scrollMask,
+  {
+    y: "100%",
+  },
+  {
+    y: "0%",
+  }
+);
+
+ScrollTrigger.create({
+  animation: tl2,
+  trigger: expertise,
+  start: "top-=300 top",
+  end: "bottom bottom",
+  markers: true,
+  scrub: true,
 });
