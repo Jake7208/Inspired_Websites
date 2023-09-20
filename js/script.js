86aa19b72a518707dbe606c7e7f16dfc2c611bab
Jake7208 <jake7208@gmail.com>
2023-09-20 00:43:43 -0600

    nearly finished page

--- js/script.js
+++ js/script.js
@@ -9,8 +9,10 @@ requestAnimationFrame(raf);
 
 gsap.registerPlugin(ScrollTrigger);
 const mask = document.querySelectorAll(".mask p");
-const expertise = document.querySelector("#expertise");
-const scrollMask = document.querySelector(".expertise-title");
+const expertise = document.querySelectorAll("#section-cont");
+const scrollMask = document.querySelectorAll(".section-title");
+const expertise2 = document.querySelectorAll("#section-contB");
+const scrollMask2 = document.querySelectorAll(".section-titleB");
 const title = document.querySelector("#title h1");
 const nav = document.querySelector("nav");
 
@@ -76,6 +78,26 @@ ScrollTrigger.create({
   scrub: 1,
 });
 const body = document.querySelector("body");
+const tl23 = gsap.timeline();
+
+tl23.fromTo(
+  scrollMask2,
+  {
+    y: "-100%",
+  },
+  {
+    y: "0%",
+  }
+);
+
+ScrollTrigger.create({
+  animation: tl23,
+  trigger: expertise2,
+  start: "top-=100% top",
+  end: "top center",
+  // markers: true,
+  scrub: 1,
+});
 
 const tl3 = gsap.timeline({ paused: true });
 
@@ -97,7 +119,7 @@ ScrollTrigger.create({
 });
 
 const cont = document.querySelector(".content-wrapper");
-// const stats = document.querySelector("#stats");
+const stats = document.querySelector("#stats");
 const video = document.querySelector(".video");
 const videoCont = document.querySelector(".video-container");
 
@@ -117,8 +139,9 @@ const stackItems = document.querySelectorAll(".stack-item p");
 stacktl = gsap.timeline({
   scrollTrigger: {
     trigger: cont,
-    start: "top-=30% top+=15%",
+    start: "top-=500 top+=15%",
     end: "bottom+=15% bottom",
+    // markers: true,
     scrub: true,
   },
 });
@@ -132,6 +155,70 @@ stacktl.fromTo(
   {
     opacity: 1,
     y: "0%",
-    stagger: 0.2,
+    stagger: 0.8,
+    ease: Power2.easeInOut,
+  }
+);
+
+gsap.fromTo(
+  body,
+  {
+    backgroundColor: "var(--background-color)",
+    color: "var(--text-color)",
+  },
+  {
+    backgroundColor: "var(--background-trans-color)",
+    color: "var(--background-color)",
+    duration: 1,
+    scrollTrigger: {
+      trigger: expertise2,
+      start: "top-=15% top+=15%",
+      end: "bottom+=15% bottom",
+      // markers: true,
+      scrub: true,
+    },
+  }
+);
+
+const testimonial = document.querySelector(".testimonial-container");
+
+if (window.innerWidth > 800) {
+  gsap.to(".stats-container", {
+    scrollTrigger: {
+      trigger: "#stats",
+      start: "top-=80 top",
+      end: "bottom top",
+      // markers: true,
+      pin: true,
+      pinSpacing: false,
+    },
+  });
+} else {
+  gsap.to(".stats-container", {
+    scrollTrigger: {
+      trigger: ".stats:last-child",
+      start: "top-=80 top",
+      end: "bottom top",
+      // markers: true,
+      pin: true,
+      pinSpacing: false,
+    },
+  });
+}
+
+gsap.fromTo(
+  body,
+  {},
+  {
+    backgroundColor: "var(--background-color)",
+    color: "var(--text-color)",
+    scrollTrigger: {
+      trigger: "#testimonials",
+      start: "top-=80 top",
+      end: "top top",
+      scrub: true,
+      // markers: true,
+      pinSpacing: false,
+    },
   }
 );
