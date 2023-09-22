661047b5d45222d209aec569d602043d724461e4
Jake7208 <jake7208@gmail.com>
2023-09-22 01:07:20 -0600

    finished the Pans & SErvicees Page

--- js/script.js
+++ js/script.js
@@ -1,61 +1,8 @@
-const lenis = new Lenis();
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
-
 gsap.registerPlugin(ScrollTrigger);
-const mask = document.querySelectorAll(".mask p");
 const expertise = document.querySelectorAll("#section-cont");
 const scrollMask = document.querySelectorAll(".section-title");
 const expertise2 = document.querySelectorAll("#section-contB");
 const scrollMask2 = document.querySelectorAll(".section-titleB");
-const title = document.querySelector("#title h1");
-const nav = document.querySelector("nav");
-
-const loadIn = gsap.timeline({
-  defaults: { duration: 1, ease: Power2.ease },
-});
-
-loadIn.from(nav, 0.8, {
-  opacity: 0,
-  y: "-20%",
-});
-loadIn.from(title, 0.5, {
-  opacity: 0,
-  x: "-10%",
-});
-
-loadIn.from(
-  mask,
-  1,
-  {
-    y: "-100%",
-  },
-  "< 0.5"
-);
-
-const tl = gsap.timeline({ paused: true });
-const hamburger = document.querySelector("#hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const closeMenu = document.querySelector(".close-menu");
-
-tl.to(navMenu, {
-  clipPath: "polygon(100% 0, 0 0, 0 100%, 100% 100%)",
-  duration: 0.2,
-  ease: Power2.easeInOut,
-});
-
-hamburger.addEventListener("click", () => {
-  tl.play();
-});
-
-closeMenu.addEventListener("click", () => {
-  tl.reverse();
-});
 
 const tl2 = gsap.timeline();
 
